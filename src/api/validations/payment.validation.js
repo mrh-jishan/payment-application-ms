2da43acdfc1c6bd5c8f02cc584df484084f5a0f4
Md Robiul Hassan <robiul.hassan@finterra.org>
2019-08-05 04:05:47 +0800

    fix

--- src/api/validations/payment.validation.js
+++ src/api/validations/payment.validation.js
@@ -9,7 +9,7 @@ module.exports = {
     body: {
       cardHolderName: Joi.string().required(),
       cardNumber: Joi.string().min(10).max(20).required(),
-      expiryMonth: Joi.string().regex(/^(0[1-9]|1[0-2]|[1-9])\/(1[4-9]|[2-9][0-9]|20[1-9][1-9])$/),
+      expiryDate: Joi.string().regex(/^(0[1-9]|1[0-2]|[1-9])\/(1[4-9]|[2-9][0-9]|20[1-9][1-9])$/),
       cvc: Joi.string().required().min(3).max(4),
       createdBy: Joi.string().required().max(128),
       product: Joi.string().required().max(128),
